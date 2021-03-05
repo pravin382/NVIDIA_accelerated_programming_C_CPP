de90ea73bd37e904abe5d8c9cfc09a62c518a0af
Pravin Kumar Jaisawal <udayapur.pravin@gmail.com>
2021-03-05 23:10:22 +0100

    Add minor change to first-parallel program

--- Solutions/01-first-parallel.cu
+++ Solutions/01-first-parallel.cu
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-/*
- * Refactor firstParallel so that it can run on the GPU.
- */
-
 __global__ void firstParallel()
 {
   printf("This should be running in parallel.\n");
@@ -15,7 +11,7 @@ int main()
   // run firstParallel in 4 thread blocks each containing 5 threads
   firstParallel<<<4, 5>>>();
 
-  // wait until 
+  // wait until gpu operations are complete
   cudaDeviceSynchronize();
 
 }
